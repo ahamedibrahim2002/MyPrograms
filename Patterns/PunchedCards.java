951d283a2fc28f0eeff6565333e8de23bc5e38a1
ahamedibrahim <ahamedibrahim2002@gmail.com>
2022-06-15 14:04:27 +0530

    efficient solution than before

--- Patterns/PunchedCards.java
+++ Patterns/PunchedCards.java
@@ -1,71 +1,88 @@
 /*
    https://codingcompetitions.withgoogle.com/codejam/round/0000000000876ff1/0000000000a4621b
 */
-
 import java.io.*;
 public class PunchedCards{  
-    static boolean dotPrint=true;
-
-    public static void main(String[] args) {
-       
-      try{   
-       InputStreamReader in=new InputStreamReader(System.in);
-       BufferedReader br=new BufferedReader(in);
     
-       int n=Integer.parseInt(br.readLine());
-       for (int i=1; i<=n; i++) {
+    public static void main(String[] args) {   
+    
+      //take input  
+      try{   
+          InputStreamReader in=new InputStreamReader(System.in);
+          BufferedReader br=new BufferedReader(in);
+    
+          int n=Integer.parseInt(br.readLine());
+          
+          for (int i=1; i<=n; i++) {
 
            String[] inputs=br.readLine().split(" ");
       
-           int row=Integer.parseInt(inputs[0]);
-           int col=Integer.parseInt(inputs[1]);
+           int r=Integer.parseInt(inputs[0]);
+           int c=Integer.parseInt(inputs[1]);
       
            System.out.println("Case #"+i+":");
-      
-           // initialPrinter(col);
-           //prints --> ..+ at first row top left
-           System.out.print(".");
-           for (int j=0; j<col; j++){
-             if (j!=0) {
-               System.out.print("-+");
-             }else{
-               System.out.print(".+");
-           }
-           }
-           System.out.println("");
-           for (int j=0; j<row; j++) {
-              finalPrinter(col);
-           }
-       }
+           //solve
+           solve(r,c);
+         }
       }catch(IOException e){
         System.out.println(e);
       }
-    } 
-    // static void initialPrinter(int col){
-        
-    // }
-    static void finalPrinter(int col){
-       
-         
-       //prints --> |.| or ..|
-       for (int i=0; i<col; i++){
-          if(i==0){
-            if(dotPrint){
-              System.out.print(".");
-              dotPrint=false;
-            }
-            else System.out.print("|");
-          }
-          System.out.print(".|");
-       }
 
-       System.out.println("");
-       
-       //prints --> +-+
-       for (int i=0; i<=col; i++){
-          if(i==0) System.out.print("+");
-          else System.out.print("-+");
-       }
-       System.out.println("");
+
     }
-}
+    static void solve(int R,int C){
+      
+      for(int row=1;row<=2*R+1;++row){
+        for(int col=1;col<=2*C+1;++col){
+             if(row <=2 && col<=2){
+               System.out.print(".");   //special case
+             }
+             else if(row%2!=0){         
+               if(col%2!=0)         
+                System.out.print("+");
+               else
+                System.out.print("-");
+             }else{
+               if(col%2!=0)
+                 System.out.print("|");
+               else
+                 System.out.print(".");
+             }
+        }
+        System.out.println();
+      }
+
+    }
+} 
+
+/*
+input:
+
+3
+3 4
+2 2
+2 3
+
+output:
+
+Case #1:
+..+-+-+-+
+..|.|.|.|
++-+-+-+-+
+|.|.|.|.|
++-+-+-+-+
+|.|.|.|.|
++-+-+-+-+
+Case #2:
+..+-+
+..|.|
++-+-+
+|.|.|
++-+-+
+Case #3:
+..+-+-+
+..|.|.|
++-+-+-+
+|.|.|.|
++-+-+-+
+*/
